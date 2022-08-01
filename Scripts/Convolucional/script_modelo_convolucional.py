b380e09378872d0b93b248a3799f40070a027ef8
Nicolas Pino Bruce <npino@ui01.hpc.utfsm.cl>
2022-08-01 12:01:48 -0400

    Results from experiments

--- Scripts/Convolucional/script_modelo_convolucional.py
+++ Scripts/Convolucional/script_modelo_convolucional.py
@@ -9,28 +9,52 @@
 import numpy as np
 import pickle
 import os
+import bz2
 
-
-with open("./X_train","rb") as arc:
+'''
+with open("X_train","rb") as arc:
     X_train = pickle.load(arc)
 
-with open("./X_Test","rb") as arc:
+with open("X_test","rb") as arc:
     X_test = pickle.load(arc)
+'''
 
-with open("./y_train","rb") as arc:
+with open("y_train","rb") as arc:
     y_train = pickle.load(arc)
 
-with open("./y_test","rb") as arc:
+with open("y_test","rb") as arc:
     y_test = pickle.load(arc)
 
+ifile = bz2.BZ2File("X_train","rb")
+X_train = pickle.load(ifile)
+ifile.close()
+
+ifile = bz2.BZ2File("X_test","rb")
+X_test = pickle.load(ifile)
+ifile.close()
+
+'''
+ifile = bz2.BZ2File("y_train","rb")
+y_train = pickle.load(ifile)
+ifile.close()
+
+ifile = bz2.BZ2File("y_test","rb")
+y_test = pickle.load(ifile)
+ifile.close()
+'''
 categorias = y_train.shape[1]
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
 
 from tensorflow.keras import layers, models
 
 
 # In[2]:
 
-batch_size = 128
+batch_size = 16
 
 units = 64
 output_size = 10 
@@ -41,10 +65,13 @@ lr = 5e-3
 
 def crearRedConvolusional(forma,categorias):
     model = models.Sequential()
-    model.add(layers.Conv2D(16, (3, 3), strides=1,activation='relu', data_format="channels_first", input_shape=forma))
-    model.add(layers.MaxPooling2D((2, 2), strides=1))
-    model.add(layers.Conv2D(32, (3, 3), strides=1, data_format="channels_first", activation='relu'))
+    model.add(layers.InputLayer(input_shape=forma))
+    model.add(layers.Conv2D(16, 3, activation='relu', data_format="channels_first"))
+    model.add(layers.MaxPooling2D((2, 2))
+    model.add(layers.Conv2D(32, 3,data_format="channels_first", activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(32,3,data_format="channels_first", activation="relu"))
+    model.add(layers.MaxPooling2D((2,2)))
     model.add(layers.Flatten())
     #model.add(layers.Dense(1, activation='relu'))
     model.add(layers.Dense(categorias, activation="softmax"))
@@ -63,9 +90,9 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.callbacks import ModelCheckpoint
 import tensorflow.keras as ks
 
-ruta_modelo = "./modelo_entrenado_convolucional"
-checkpoint_best = ModelCheckpoint(filepath=ruta_modelo, monitor='val_loss',verbose=1, save_best_only=True, mode='min')
-lrschedule_1 = ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.70, mode='min')
+ruta_modelo = "../../Modelo_convolucional/modelo_entrenado_convolucional"
+checkpoint_best = ModelCheckpoint(filepath=ruta_modelo, monitor='val_loss',verbose=1, save_best_only=True, mode='auto')
+lrschedule_1 = ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.70, mode='auto')
 
 modelo.compile(loss='categorical_crossentropy', 
                      optimizer=ks.optimizers.Adam(lr=lr, decay=5e-4), 
@@ -75,12 +102,12 @@ modelo.compile(loss='categorical_crossentropy',
 historia= modelo.fit(
             x=X_train, 
             y=y_train, 
-            batch_size=batch_size, 
+            batch_size=16, 
             epochs=50, 
             verbose=True, 
             validation_data=(X_test, y_test),
             callbacks=[checkpoint_best, lrschedule_1])
 
 
-with open('./historiañ_convolucional', 'wb') as file_pi:
+with open('../../Modelo_convolucional/historial_convolucional', 'wb') as file_pi:
     pickle.dump(historia.history, file_pi)
