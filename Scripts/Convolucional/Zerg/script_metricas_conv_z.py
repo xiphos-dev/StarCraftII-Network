1705fbca63fa5c7c7d4271f432940644f1e040fc
nicolaspino <benjamin.camblor@sansano.usm.cl>
2022-09-29 14:30:52 -0300

    Script for result validation

--- Scripts/Convolucional/Zerg/script_metricas_conv_z.py
+++ Scripts/Convolucional/Zerg/script_metricas_conv_z.py
@@ -1,67 +1,61 @@
 import os
 import pickle
 import numpy as np
+import bz2
 
-with open("./X_train","rb") as arc:
-    X_train = pickle.load(arc)
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
 
-with open("./X_test","rb") as arc:
-    X_test = pickle.load(arc)
+with open("encoder","rb") as arc:
+    encoder = pickle.load(arc)
 
-with open("./y_train","rb") as arc:
-    y_train = pickle.load(arc)
+def producirDatosMatriz(predicciones, y_test):
+    
+    y_pred = np.argmax(predicciones, axis=-1)
 
-with open("./y_test","rb") as arc:
-    y_test = pickle.load(arc)
+    y_true=np.argmax(y_test, axis=-1)
 
+    cm = confusion_matrix(y_true, y_pred)
+    
+    precision = precision_score(y_true, y_pred, average="weighted")
+    recall = recall_score(y_true, y_pred, average="weighted")
+    f1 = f1_score(y_true, y_pred, average='weighted')
+    accuracy = accuracy_score(y_true, y_pred)
+    
+    return precision, recall, f1, accuracy, cm
 
-categorias = y_train.shape[1]
+from tensorflow import keras
+ruta_modelo = "./Modelo88f_sinpadding_z"
+modelo =  keras.models.load_model(ruta_modelo)
 
-from tensorflow.keras import layers, models
+numero_secciones_data = 2
 
+for numero in range(1,numero_secciones_data+1):
 
-def crearRedConvolusional(forma,categorias):
-    model = models.Sequential()
-    model.add(layers.Conv2D(16, (3, 3), strides=1,activation='relu', data_format="channels_first", input_shape=forma))
-    model.add(layers.MaxPooling2D((2, 2), strides=1))
-    model.add(layers.Conv2D(32, (3, 3), strides=1, data_format="channels_first", activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1, activation='relu'))
-    model.add(layers.Dense(categorias, activation="softmax"))
-    return model
+    with open("y_test_"+numero,"rb") as arc:
+        y_test = pickle.load(arc)
 
+    ifile = bz2.BZ2File("X_test_"+numero,"rb")
+    X_test = pickle.load(ifile)
+    ifile.close()
 
-modelo = crearRedConvolusional(X_train.shape[1:],categorias)
+    predicciones = modelo_denso.predict(X_test)
 
-y_train = np.asarray(y_train)
-y_test = np.asarray(y_test)
+    precision, recall, f1, acc, cm = producirDatosMatriz(predicciones, y_test)
 
+    estado = {
+        "precision": precision,
+        "recall": recall,
+        "accuracy": acc,
+        "fscore": f1,
+        "cm": cm,
+        "clases": encoder.classes_,
+        "y_pred": predicciones,
+        "y_test": y_test
+    }
 
-from tensorflow.keras.callbacks import ReduceLROnPlateau
-from tensorflow.keras.callbacks import ModelCheckpoint
-import tensorflow.keras as ks
-
-
-ruta = os.path.join(os.environ["SLURM_SUBMIT_DIR"],"Modelo_convolucional")
-checkpoint_best = ModelCheckpoint(filepath=ruta, monitor='val_loss',verbose=1, save_best_only=True, mode='min')
-lrschedule_1 = ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.70, mode='min')
-
-modelo.compile(loss='categorical_crossentropy', 
-                     optimizer=ks.optimizers.Adam(lr=0.003, decay=5e-4), 
-                     #metrics=[tf.keras.metrics.CategoricalCrossentropy()]
-                    )
-
-historia= modelo.fit(
-            x=X_train, 
-            y=y_train, 
-            batch_size=16, 
-            epochs=50, 
-            verbose=True, 
-            validation_data=(X_test, y_test),
-            callbacks=[checkpoint_best, lrschedule_1])
-
-
-ruta = os.path.join(os.environ["SLURM_SUBMIT_DIR"],"trainHistoryDict")
-with open('ruta', 'wb') as file_pi:
-    pickle.dump(historia.history, file_pi)
+    with open('./Validaciones/metricas_'+numero, 'wb') as file_pi:
+        pickle.dump(estado, file_pi)
